25e7f24ebecd21f53d0d9b0079bc471bad4f2adc
CdtDelta <cdtdelta@gmail.com>
2015-02-08 22:06:25 -0600

    YOP - Week Six Script

---
+++ yop-week6.py
@@ -0,0 +1,17 @@
+from configobj import ConfigObj
+import os
+import sys
+
+config_file = sys.argv[1]
+
+if os.path.exists(config_file):
+    print "File exists"
+    config = ConfigObj(config_file)
+    vxshare_no = int(config['last_vxshare_num'])
+else:
+    print "File doesn't exist"
+    config = ConfigObj()
+    config.filename = config_file
+    config['last_vxshare_num'] = 0
+    vxshare_no = int(config['last_vxshare_num'])
+    config.write()
