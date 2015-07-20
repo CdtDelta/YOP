d5dfe757d2bcc3a7adeb82bffe08fe750d0ec7d4
CdtDelta <cdtdelta@gmail.com>
2015-07-19 21:45:04 -0500

    YOP - Week Twenty Nine Script

--- yop-week29.py
+++ yop-week29.py
@@ -6,22 +6,24 @@ import random
 import os.path
 import string
 
+# This is the function to generate the random FV key from Week 11 script
 def fvde_key():
     chars = string.letters.upper() + string.digits
     pwdSize = 4
     key_attempt = ''.join((random.choice(chars)) for x in range(pwdSize))
     return key_attempt
 
+# This function checks the generated key to see if we've seen it before
 def check_key(key_select, db):
     conn = sqlite3.connect(db)
     c = conn.cursor()
     c.execute("select * from tracking where enc_key = ( ? );", (key_select,))
     enc_key_exists = c.fetchone()
     if enc_key_exists:
-        print "{} already tried...skipping...".format(key_select)
+        print "{} already tried...skipping...".format(key_select) # The key has been tried already move on
         return False
     else:
-	print "Adding key {}...".format(key_select)
+	print "Adding key {}...".format(key_select) # It's a new key, lets add it to the DB
         c.execute("insert into tracking values(NULL, ? );", (key_select,))
 	conn.commit()
         return True
@@ -30,6 +32,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', dest='db_name', required=True, help='Name of database file to track keys')
 args = parser.parse_args()
 
+# This just checks if the db file in the command line exists or not
 if os.path.isfile(args.db_name):
     print "{} exists...".format(args.db_name)
 else:
@@ -39,6 +42,7 @@ else:
     
 locked = True
 
+# This is a variation of the week 11 script, it'll be update to incorporate the Week 11 code loop
 while locked == True:
     decryption_key = "-".join([fvde_key(),fvde_key(),fvde_key(),fvde_key(),fvde_key(),fvde_key()])
     print "Checking decryption key {}...".format(decryption_key)
