2bb1b5a67a196a35a7970df7fa146c03e7a21ff8
Tom Yarrish <cdtdelta@gmail.com>
2016-04-25 01:47:24 +0000

    Bug fixes; Changed participation to quiz

--- yop-week18.py
+++ yop-week18.py
@@ -14,19 +14,19 @@ midterm_total = raw_input("Enter the Midterm grade: ")
 final_total = raw_input("Enter the Final Grade: ")
 lab_total = raw_input("Enter the Lab Total Grade: ")
 homework_total = raw_input("Enter the Homework Total Grade: ")
-participation_total = raw_input("Enter the Participation Grade: ")
+quiz_total = raw_input("Enter the Quiz Total Grade: ")
 
 midterm_calc = float(midterm_total) * 0.2
 final_calc = float(final_total) * 0.2
 lab_calc = float(lab_total) * 0.3
 homework_calc = float(homework_total) * 0.2
-participation_calc = float(participation_total) * 0.1
+quiz_calc = float(quiz_total) * 0.1
 
 extra_credit = raw_input("Enter extra credit score (Enter if none): ")
 
 if extra_credit:
-    grade = (midterm_calc + final_calc + lab_calc + homework_calc + participation_calc) + int(extra_credit)
+    grade = (midterm_calc + final_calc + lab_calc + homework_calc + quiz_calc) + int(extra_credit)
 else:
-    grade = midterm_calc + final_calc + lab_calc + homework_calc + participation_total
+    grade = (midterm_calc + final_calc + lab_calc + homework_calc + quiz_calc)
 
 print "Grade is {}\n".format(int(grade))
